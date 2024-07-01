f7a3835ce0fe21576a08ed136cd7850b13339c58
EggSaled <adrian.escobar.104183@gmail.com>
2024-07-01 10:40:43 -0700

    minor changes to treesitter configs

--- lua/eggsaled/treesitter.lua
+++ lua/eggsaled/treesitter.lua
@@ -6,11 +6,10 @@ end
 configs.setup({
 	ensure_installed = { "javascript", "c", "cpp", "vim", "vimdoc", "java", "json", "json5", "lua", "python", "sql", "swift", "typescript" },
 	sync_install = false,
-	ignore_install = { "" },
+	auto_install = false,
 	highlight = {
 		enable = true,
-		disable = { "" },
-		additional_vim_regex_highlighting = true,
+		additional_vim_regex_highlighting = false,
 	},
 	indent = {
 		enable = true,
