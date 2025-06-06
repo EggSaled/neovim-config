b0dd807712c51f5caeb34a2fec6e7916667d4189
EggSaled <adrian.escobar.104183@gmail.com>
2025-06-06 14:58:08 -0700

    feat: added opts to treesitter plugin

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,4 +3,17 @@ return {
 	lazy = false,
 	branch = "main",
 	build = ":TSUpdate",
+	opts = {
+		ensure_installed = { "javascript", "c", "cpp", "vim", "vimdoc", "java", "json", "json5", "lua", "python", "sql", "swift", "typescript" },
+		sync_install = false,
+		auto_install = false,
+		highlight = {
+			enable = true,
+			additional_vim_regex_highlighting = false,
+		},
+		indent = {
+			enable = true,
+			disable = { "yaml" },
+		},
+	}
 }
