ec31e5df49052019aba978d5f0bc29e3dcc72541
EggSaled <adrian.escobar.104183@gmail.com>
2025-06-13 18:16:03 -0700

    chore: removed unused imports in init.lua

--- init.lua
+++ init.lua
@@ -1,12 +1,5 @@
 require "eggsaled.options"
 require "eggsaled.keymaps"
--- require "eggsaled.colorscheme"
--- require "eggsaled.cmp"
--- require "eggsaled.lsp"
 require "eggsaled.lazy"
--- require "eggsaled.treesitter"
--- require "eggsaled.gitsigns"
--- require "eggsaled.nvim-tree"
--- require "eggsaled.bufferline"
 
 vim.cmd[[ set background=dark ]]
