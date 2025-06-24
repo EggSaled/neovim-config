d3b80efb36b540e9879668679858ba814c64edd2
EggSaled <adrian.escobar.104183@gmail.com>
2025-06-24 11:39:44 -0700

    feat: updated treesitter plugin config

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,28 +3,21 @@ return {
 	lazy = false,
 	branch = "main",
 	build = ":TSUpdate",
-	opts = {
-		ensure_installed = { "javascript", "c", "cpp", "vim", "vimdoc", "java", "json", "json5", "lua", "python", "sql", "swift", "typescript" },
-		sync_install = false,
-		auto_install = false,
-		highlight = {
-			enable = true,
-			additional_vim_regex_highlighting = false,
-		},
-		indent = {
-			enable = true,
-			disable = { "yaml" },
-		},
-	}
-}
--- NOTE: copied from options, not sure if this still works.
+	config = function(args)
+		local config = require('nvim-treesitter.config')
 
---[[ WORKAROUND FOR TREESITTER FOLDS
-  vim.api.nvim_create_autocmd({ 'BufEnter', 'BufAdd', 'BufNew', 'BufNewFile', 'BufWinEnter' }, {
-	  group = vim.api.nvim_create_augroup('TS_FOLD_WORKAROUND', {}),
-	  callback = function()
-		  vim.opt.foldmethod = 'expr'
-		  vim.opt.foldexpr = 'nvim_treesitter#foldexpr()'
-	  end
-  })
---]]
+		config.setup({
+			ensure_installed = { "javascript", "c", "cpp", "vim", "vimdoc", "java", "json", "json5", "lua", "python", "sql", "swift", "typescript" },
+			sync_install = false,
+			auto_install = false,
+			highlight = {
+				enable = true,
+				additional_vim_regex_highlighting = false,
+			},
+			indent = {
+				enable = true,
+				disable = { "yaml" },
+			},
+		})
+	end,
+}
